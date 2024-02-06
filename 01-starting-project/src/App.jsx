9dbccda16db3d095b82d219e6947e1d62bb56337
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-02-06 18:15:57 +0700

    Cantik banget

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,9 @@
+import reactImg from './assets/react-core-concepts.png'
+import componentsImg from './assets/components.png'
+import jsxImg from './assets/jsx-ui.png'
+import configImg from './assets/config.png'
+import stateImg from './assets/state-mgmt.png'
+
 // Array berisi kata-kata yang digunakan untuk deskripsi header
 const ListWord = ['Fundamental', 'Crucial', 'Core']
 
@@ -14,7 +20,7 @@ function Header(){
   return (
     <header>
       {/* Gambar yang ditampilkan */}
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       {/* Judul Header */}
       <h1>React Essentials</h1>
       {/* Paragraf dengan deskripsi dinamis */}
@@ -26,6 +32,14 @@ function Header(){
   );
 }
 
+function CoreConcept(props){
+  return <li>
+    <img src={props.img} alt={props.title} />
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+  </li>
+}
+
 // Komponen utama App yang memanggil Header
 function App() {
   return (
@@ -33,8 +47,32 @@ function App() {
       {/* Memanggil komponen Header */}
       <Header/>
       <main>
-        {/* Judul utama */}
-        <h2>Time to get started!</h2>
+      <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+        <CoreConcept
+          title = "Components"
+          description = "The core Ut building block compose the usar interface by combining multiple"
+          img ={componentsImg}
+        />
+        <CoreConcept
+          title = "JSX"
+          description = "Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered"
+          img ={jsxImg}
+        />
+        <CoreConcept
+          title = "Props"
+          description = "Make components configurable (and therefore rausable) by passing input dato to them"
+          img ={configImg}
+        />
+        <CoreConcept
+          title = "State"
+          description = "React-managed data which, whim changed, causes the component to re render & the U to update"
+          img ={stateImg}
+        />
+
+        </ul>
+      </section>
       </main>
     </div>
   );
